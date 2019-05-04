0af75c53e4660f16c335c1e2be492b19ea24c8a5
Anuj <anuj_ramakrishnan@berkeley.edu>
2019-05-03 18:29:30 -0700

    Drive information updated

--- app/screens/Drives.js
+++ app/screens/Drives.js
@@ -3,6 +3,45 @@ import {FlatList, View} from 'react-native';
 import {DrivesCard} from '../components/CharityTabComponents/DrivesCard';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
+//The data below needs to be called using an API
+const driveData = [
+  {driveImageURL: 'https://mldpyw8anemv.i.optimole.com/w:auto/h:auto/q:auto/https://mk0geekspinexfjuv770.kinstacdn.com/wp-content/uploads/2018/11/detective-pikachu.jpg'
+, driveLocation: 'Berkeley, CA', driveTitle: 'Help Detective Pikachu'
+, driveAbout: 'There is this homeless guy sitting on Telegraph and Bancroft. We really think that we can help him out.'
+, currentMoney: '450'
+, targetMoney: '2000'
+, percentCompleted: 0.8},
+{driveImageURL: 'https://media.npr.org/assets/img/2016/10/15/gettyimages-543499144_wide-c7be8ee176c6dabe59ee7a2f2758c4633c6d1c7d-s800-c85.jpg'
+, driveLocation: 'Berkeley, CA', driveTitle: 'Help the Homeless on Telegraph'
+, driveAbout: 'There is this homeless guy sitting on Telegraph and Bancroft. We really think that we can help him out.'
+, currentMoney: '450'
+, targetMoney: '2000'
+, percentCompleted: 0.3},
+{driveImageURL: 'https://media.npr.org/assets/img/2016/10/15/gettyimages-543499144_wide-c7be8ee176c6dabe59ee7a2f2758c4633c6d1c7d-s800-c85.jpg'
+, driveLocation: 'Berkeley, CA', driveTitle: 'Help the Homeless on Telegraph'
+, driveAbout: 'There is this homeless guy sitting on Telegraph and Bancroft. We really think that we can help him out.'
+, currentMoney: '450'
+, targetMoney: '2000'
+, percentCompleted: 0.3},
+{driveImageURL: 'https://media.npr.org/assets/img/2016/10/15/gettyimages-543499144_wide-c7be8ee176c6dabe59ee7a2f2758c4633c6d1c7d-s800-c85.jpg'
+, driveLocation: 'Berkeley, CA', driveTitle: 'Help the Homeless on Telegraph'
+, driveAbout: 'There is this homeless guy sitting on Telegraph and Bancroft. We really think that we can help him out.'
+, currentMoney: '450'
+, targetMoney: '2000'
+, percentCompleted: 0.5},
+{driveImageURL: 'https://mldpyw8anemv.i.optimole.com/w:auto/h:auto/q:auto/https://mk0geekspinexfjuv770.kinstacdn.com/wp-content/uploads/2018/11/detective-pikachu.jpg'
+, driveLocation: 'Berkeley, CA', driveTitle: 'Help Detective Pikachu'
+, driveAbout: 'There is this homeless guy sitting on Telegraph and Bancroft. We really think that we can help him out.'
+, currentMoney: '450'
+, targetMoney: '2000'
+, percentCompleted: 0.8},
+{driveImageURL: 'https://mldpyw8anemv.i.optimole.com/w:auto/h:auto/q:auto/https://mk0geekspinexfjuv770.kinstacdn.com/wp-content/uploads/2018/11/detective-pikachu.jpg'
+, driveLocation: 'Berkeley, CA', driveTitle: 'Help Detective Pikachu'
+, driveAbout: 'There is this homeless guy sitting on Telegraph and Bancroft. We really think that we can help him out.'
+, currentMoney: '450'
+, targetMoney: '2000'
+, percentCompleted: 0.8}]
+
 class Drives extends Component {
 
   render() {
@@ -10,31 +49,7 @@ class Drives extends Component {
       <View>
       <FlatList
             columnWrapperStyle={styles.row}
-            data={[
-              {driveImageURL: 'https://mldpyw8anemv.i.optimole.com/w:auto/h:auto/q:auto/https://mk0geekspinexfjuv770.kinstacdn.com/wp-content/uploads/2018/11/detective-pikachu.jpg'
-            , driveLocation: 'Berkeley, CA', driveTitle: 'Help Detective Pikachu'
-            , driveAbout: 'There is this homeless guy sitting on Telegraph and Bancroft. We really think that we can help him out.'
-            , currentMoney: '450'
-            , targetMoney: '2000'
-            , percentCompleted: 0.8},
-            {driveImageURL: 'https://media.npr.org/assets/img/2016/10/15/gettyimages-543499144_wide-c7be8ee176c6dabe59ee7a2f2758c4633c6d1c7d-s800-c85.jpg'
-            , driveLocation: 'Berkeley, CA', driveTitle: 'Help the Homeless on Telegraph'
-            , driveAbout: 'There is this homeless guy sitting on Telegraph and Bancroft. We really think that we can help him out.'
-            , currentMoney: '450'
-            , targetMoney: '2000'
-            , percentCompleted: 0.3},
-            {driveImageURL: 'https://media.npr.org/assets/img/2016/10/15/gettyimages-543499144_wide-c7be8ee176c6dabe59ee7a2f2758c4633c6d1c7d-s800-c85.jpg'
-            , driveLocation: 'Berkeley, CA', driveTitle: 'Help the Homeless on Telegraph'
-            , driveAbout: 'There is this homeless guy sitting on Telegraph and Bancroft. We really think that we can help him out.'
-            , currentMoney: '450'
-            , targetMoney: '2000'
-            , percentCompleted: 0.3},
-            {driveImageURL: 'https://media.npr.org/assets/img/2016/10/15/gettyimages-543499144_wide-c7be8ee176c6dabe59ee7a2f2758c4633c6d1c7d-s800-c85.jpg'
-            , driveLocation: 'Berkeley, CA', driveTitle: 'Help the Homeless on Telegraph'
-            , driveAbout: 'There is this homeless guy sitting on Telegraph and Bancroft. We really think that we can help him out.'
-            , currentMoney: '450'
-            , targetMoney: '2000'
-            , percentCompleted: 0.5}]}
+            data={driveData}
             renderItem={({item}) => (
             <DrivesCard
               drive= {item}/>
