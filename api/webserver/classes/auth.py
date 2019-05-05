acdfce5866538e848f205a85009d7032b66cce76
9vinny <9vinny@gmail.com>
2019-05-04 19:55:46 -0700

    made db changes, connected endpoints to db

--- api/webserver/classes/auth.py
+++ api/webserver/classes/auth.py
@@ -2,6 +2,7 @@ from classes.donor import Donor
 from db.orm import *
 import logging
 from classes.sql_conn import SqlConn
+import datetime
 
 class Auth(object):
     """Class for authentication of user:
@@ -23,10 +24,17 @@ class Auth(object):
         # Have to handle the exsiting user scenarios
         try:
             db_obj=SqlConn()
-            if db_obj.user_exists(username):
+            query="Select * from donor where username = %s"
+            data = (username,)
+            result=db_obj.get_query(query,data)
+            if len(result)>0:
                 raise Exception("Username exists - Mail id already in use")
             else:
-                uid=db_obj.create_user(username,password)
+                query="Insert into donor (username, password, name, activation_date, account_status) values(%s,%s,%s,%s,%s)"
+                data = (username,password,"Donor",datetime.datetime.now(),True,)
+                db_obj.set_query(query,data)
+                result= db_obj.get_query("Select donor_id from donor where username = %s",(username,))
+                uid=result[0][0]
                 user = Donor(uid)
                 return user, True
 
@@ -44,15 +52,17 @@ class Auth(object):
 
         try:
             db_obj=SqlConn()
-            uid, validation_flag=db_obj.check_user(username,password)
+            query="Select * from donor where username = %s and password= %s"
+            data = (username,password,)
+            result=db_obj.get_query(query,data)
+            uid=0
             user=None
-            if validation_flag:
+            if len(result)>0 and result[0][-1]==1 :
+                uid=result[0][0]
                 user=Donor(uid)
-
                 print (uid,user)
                 self.status=True
             return user, self.status
-
         except Exception as e:
             logging.info(e)
             raise
@@ -61,31 +71,34 @@ class Auth(object):
 
     def update_password(self, username,password,new_password):
         """Change credentials for existing user, returns user object with valid id."""
-
-        try:
-            db_obj=SqlConn()
-            uid, validation_flag=db_obj.check_user(username,password)
-            if validation_flag:
-                user=Donor(uid)
-                self.status=user.set_password(new_password)
-            return self.status
-        except Exception as e:
-            logging.info(e)
-            raise
-        finally:
-            db_obj.close_conn()
+        pass
+        # try:
+        #     db_obj=SqlConn()
+        #     uid, validation_flag=db_obj.check_user(username,password)
+        #     if validation_flag:
+        #         user=Donor(uid)
+        #         self.status=user.set_password(new_password)
+        #     return self.status
+        # except Exception as e:
+        #     logging.info(e)
+        #     raise
+        # finally:
+        #     db_obj.close_conn()
 
     def reset_password(self, username):
         """Change credentials for existing user, returns user object with valid id."""
 
         try:
             db_obj=SqlConn()
-            validation_flag=db_obj.check_user(username,password)
-            if validation_flag:
-                user=Donor(uid)
-                self.status=user.set_password("CharityWallet#123")
+            query="Select * from donor where username = %s"
+            data = (username,)
+            result=db_obj.get_query(query,data)
+            if len(result)>0:
+                #update pwd in db
                 #send pwd in mail
-            return self.status
+                return True
+            else:
+                raise Exception("Username does not exist")
         except Exception as e:
             logging.info(e)
             raise
--- api/webserver/classes/donor.py
+++ api/webserver/classes/donor.py
@@ -1,4 +1,6 @@
 import datetime;
+from classes.sql_conn import SqlConn
+
 class Donor(object):
     """Donor class. Donors have the
     following properties:
@@ -12,17 +14,25 @@ class Donor(object):
     """
 
     def __init__(self,uid=""):
-        """Return a Customer object whose name is *name* and starting
-        balance is *balance*."""
+        """Return a Donor object """
 
         self.name = "Donor"
         self.uid=uid
-        self.last_logged_in = datetime.datetime.now()
         self.fav_causes =[]
         self.username=""
         self.password=""
+        self.lifetime_donation=0
+        self.monthly_collected=0
+        self.causes=[]
+        print("donor constructor")
+        self.sync_from_db()
 
-        # self.sync_from_db()
+    def sync_from_db(self):
+        """Fetches user profile details"""
+        pass
+
+    def sync_to_db(self):
+        pass
 
     def set_profile(self, name, fav_causes):
         """Saves user profile details"""
@@ -32,8 +42,42 @@ class Donor(object):
 
         try:
             db_obj=SqlConn()
-            validation_flag=db_obj.update_donor_profile(self)
-            return validation_flag
+
+        except Exception as e:
+            logging.info(e)
+            raise
+        finally:
+            db_obj.close_conn()
+
+    def get_profile(self, name, fav_causes):
+        pass
+
+
+
+    def get_id(self):
+        """Fetches user id"""
+        print("get_id",self.uid)
+        return self.uid
+
+    def get_user_totals(self):
+        print("in user tots")
+        try:
+            db_obj=SqlConn()
+
+            query="Select lifetime_donation, monthly_collected,  from donor \
+             where donor_id = %s"
+            data = (self.uid,)
+            result = db_obj.get_query(query,data)
+
+            query="Select count(distinct drive_id), \
+            count(distinct charity_id) from donor_drive \
+             where donor_id = %s"
+            
+            result2 = db_obj.get_query(query,data)
+
+            return {'user_id':self.uid,'month_total':result[0][1],'lifetime_total':result[0][0],'active_drives':result2[0][0],'active_charities':result2[0][1]}
+
+
         except Exception as e:
             logging.info(e)
             raise
@@ -41,11 +85,22 @@ class Donor(object):
             db_obj.close_conn()
 
 
-    def sycn_from_db(self):
-        """Fetches user profile details"""
-        return self
 
-    def get_id(self):
-        """Fetches user id"""
-        print("get_id",self.uid)
-        return self.uid
+
+    def set_access_token(self, item_id, access_token):
+        #update in obj and save to db
+
+        try:
+            db_obj=SqlConn()
+            query="Update donor set plaid_item_id= %s, plaid_access_token=%s, last_logged_in=%s  where donor_id = %s"
+            data = (item_id,access_token, datetime.datetime.now(),self.uid,)
+            db_obj.set_query(query,data)
+            self.plaid_item=item_id
+            self.plaid_access_token=access_token
+            return True
+
+        except Exception as e:
+            logging.info(e)
+            raise
+        finally:
+            db_obj.close_conn()
