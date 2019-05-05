d65c6f84ce9ef629ab3c4f7a73c4b3e204e8b9f1
Anuj <anuj_ramakrishnan@berkeley.edu>
2019-05-05 12:05:34 -0700

    wroking on changingIcon

--- app/screens/changingIcon.js
+++ app/screens/changingIcon.js
@@ -21,33 +21,21 @@ export default class ReactNativeAnimatedIcons extends Component {
 
   }
 
+    onPressHearts = (item) => {
+      // console.log(" onPress:item ",item);
+      if(!item)return
+      // item.isActive!=item.isActive
+    let {hearts} = this.state
+      let updatedlist=hearts.map(o => o.id === item.id
+                            ?{ ...o, isActive: o.isActive?false:true}
+                          :o)
+      this.setState({
+        triggerAnimationId:hearts.find(x => x.id === item.id).id,
+        hearts: updatedlist})
 
-onPressTweets = (item) => {
-  // console.log(" onPress:item ",item);
-  if(!item)return
-  // item.isActive!=item.isActive
-let {tweets} = this.state
-  let updatedlist=tweets.map(o => o.id === item.id
-                        ?{ ...o, isActive: o.isActive?false:true}
-                      :o)
-  this.setState({
-    triggerAnimationId:tweets.find(x => x.id === item.id).id,
-    tweets: updatedlist})
+    }
 
-}
-onPressHearts = (item) => {
-  // console.log(" onPress:item ",item);
-  if(!item)return
-  // item.isActive!=item.isActive
-let {hearts} = this.state
-  let updatedlist=hearts.map(o => o.id === item.id
-                        ?{ ...o, isActive: o.isActive?false:true}
-                      :o)
-  this.setState({
-    triggerAnimationId:hearts.find(x => x.id === item.id).id,
-    hearts: updatedlist})
 
-}
 
 
 
@@ -56,25 +44,33 @@ let {hearts} = this.state
     let red="rgba(245,60,60,0.8)"
     let light="rgba(255,255,255,0.5)"
   return (
-      <View style={styles.container}>
-    <TouchableOpacity
-            style={{position:"absolute",top:25,right:10,flex:1,zIndex:3,width:35,height:35,alignItems:"center",justifyContent:"center",backgroundColor:"transparent"}}>
-            <Icon
-//               onPress={()=>{
-// this.state.hearts.map((o,i) => {
-// this.setState({triggerAnimationId:0})
-// })
-//               }}
-//               disableAnimation
-              // name={"heart-outline"}
-              name={"close"}
-              // triggerAnimationId={this.state.triggerAnimationId}
-              size={25} color="rgba(256,256,256,0.5)"/>
-    </TouchableOpacity>
+    <View style={{alignItems: "center",justifyContent: "center",height:350,flexDirection:"column", }}>
+      <Text style={{textAlignVertical: "center", textAlign: "center",fontSize:20 ,margin:10}}>
+      Animate on Icon Name Change
+      </Text>
+      <View style={{height:70,alignItems: "center",justifyContent: "center",flexDirection:"row",}}>
 
+            {hearts.map((o,i) => {
 
+              return   (<TouchableOpacity style={{height:50,}} key={i} onPress={()=>this.onPressHearts(o)}>
+                <Icon
+                  item={o}
+                  name={o.isActive?"heart":"heart-outline"}
+                  // name={"heart"}
+                  isActive={o.isActive}
+                  colorOutputRange={[
+                  light,
+                  "pink",
+                   o.isActive?red:light,
+                ]}
+                // animateAllActive
+                colorInputRange={[0, 0.56, 1]}
+              />
+             </TouchableOpacity>)
 
-      </View>
+            })}
+    </View>
+    </View>
     );
   }
 }
