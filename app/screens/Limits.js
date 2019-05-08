b9e7e4ad854817d8a5eb88f7b94ee1d6c439caa7
Anuj <anuj_ramakrishnan@berkeley.edu>
2019-05-07 18:29:20 -0700

    Styling for Limits screen completed.

--- app/screens/Limits.js
+++ app/screens/Limits.js
@@ -30,61 +30,83 @@ class Limits extends Component {
     const dataRoundingMethod = [
           { key: index_1++, label: 'Round-Up' },
           { key: index_1++, label: 'Round-Down' },
-          { key: index_1++, label: 'Round to the nearest dollar' },
+          { key: index_1++, label: 'Round to the Nearest Dollar' },
          ];
     return(
-      <View style={{flex:1, flexDirection:'column'}}>
-        <Text>How often would you like to donate?</Text>
-        <ModalSelector
-                    data={data}
-                    initValue={this.state.donationFrequency}
-                    accessible={true}
-                    scrollViewAccessibilityLabel={'Scrollable options'}
-                    cancelButtonAccessibilityLabel={'Cancel Button'}
-                    onChange={(option)=>{ this.setState({donationFrequency:option.label})}}
-                    cancelText={'Close'}
-                    >
-                    <TextInput
-                        style={{borderWidth:1, borderColor:'#ccc', padding:10, height:40}}
-                        editable={false}
-                        value={this.state.donationFrequency} />
-                </ModalSelector>
+      <View style={styles.container}>
+        <View style={styles.itemContainer}>
+          <Text style={styles.questionStyle}>How often would you like to donate?</Text>
+          <ModalSelector
+            data={data}
+            initValue={this.state.donationFrequency}
+            accessible={true}
+            scrollViewAccessibilityLabel={'Scrollable options'}
+            cancelButtonAccessibilityLabel={'Cancel Button'}
+            onChange={(option)=>{ this.setState({donationFrequency:option.label})}}
+            cancelText={'Close'}
+            >
+            <TextInput
+                style={styles.textInput}
+                editable={false}
+                value={this.state.donationFrequency} />
+          </ModalSelector>
+      </View>
+      <View style={styles.itemContainer}>
+        <Text style={styles.questionStyle}>Maximum amount to be donated every cycle?</Text>
+        <View style={styles.itemSubContainer}>
+          <View style={styles.sliderContainer}>
+            <Slider
+              value={this.state.maxAmountValue}
+              step={5}
+              onValueChange={maxAmountValue => this.setState({ maxAmountValue })}
+              maximumValue={100}
+              minimumValue={5}
+              minimumTrackTintColor='#258895'
+              thumbTintColor='#258895'
+            />
+          </View>
+          <Text style={styles.valueStyle}>${this.state.maxAmountValue}</Text>
+        </View>
+      </View>
 
+      <View style={styles.itemContainer}>
+        <Text style={styles.questionStyle}>Minimum amount to be donated every cycle?</Text>
+        <View style={styles.itemSubContainer}>
+          <View style={styles.sliderContainer}>
+            <Slider
+              value={this.state.minAmountValue}
+              step={5}
+              onValueChange={minAmountValue => this.setState({ minAmountValue })}
+              maximumValue={100}
+              minimumValue={5}
+              minimumTrackTintColor='#258895'
+              thumbTintColor='#258895'
+            />
+          </View>
+          <Text style={styles.valueStyle}>${this.state.minAmountValue}</Text>
+        </View>
+      </View>
 
-        <Text>Maximum amount to be donated every cycle?</Text>
-        <Slider
-          value={this.state.maxAmountValue}
-          step={5}
-          onValueChange={maxAmountValue => this.setState({ maxAmountValue })}
-          maximumValue={100}
-          minimumValue={5}
-          minimumTrackTintColor='#258895'
-          thumbTintColor='#258895'
-        />
-        <Text>Value:$ {this.state.maxAmountValue}</Text>
-        <Text>Minimum amount to be donated every cycle?</Text>
-        <Slider
-          value={this.state.minAmountValue}
-          step={5}
-          onValueChange={minAmountValue => this.setState({ minAmountValue })}
-          maximumValue={100}
-          minimumValue={5}
-          minimumTrackTintColor='#258895'
-          thumbTintColor='#258895'
-        />
-        <Text>Value:$ {this.state.minAmountValue}</Text>
-        <Text>Minimum tx amount above which change should be collected? [Slider/text input]</Text>
-        <Slider
-          value={this.state.minTxValue}
-          step={1}
-          onValueChange={minTxValue => this.setState({ minTxValue })}
-          maximumValue={10}
-          minimumValue={0}
-          minimumTrackTintColor='#258895'
-          thumbTintColor='#258895'
-        />
-        <Text>Value:$ {this.state.minTxValue}</Text>
-        <Text>How would you like to collect change?</Text>
+      <View style={styles.itemContainer}>
+        <Text style={styles.questionStyle}>Minimum transaction amount above which change should be collected into Charity Wallet?</Text>
+        <View style={styles.itemSubContainer}>
+          <View style={styles.sliderContainer}>
+            <Slider
+              value={this.state.minTxValue}
+              step={1}
+              onValueChange={minTxValue => this.setState({ minTxValue })}
+              maximumValue={10}
+              minimumValue={0}
+              minimumTrackTintColor='#258895'
+              thumbTintColor='#258895'
+            />
+          </View>
+          <Text style={styles.valueStyle}>${this.state.minTxValue}</Text>
+        </View>
+      </View>
+
+      <View style={styles.itemContainer}>
+        <Text style={styles.questionStyle}>How would you like to collect change?</Text>
         <ModalSelector
                     data={dataRoundingMethod}
                     initValue={this.state.roundingMethod}
@@ -95,13 +117,65 @@ class Limits extends Component {
                     cancelText={'Close'}
                     >
                     <TextInput
-                        style={{borderWidth:1, borderColor:'#ccc', padding:10, height:40}}
+                        style={styles.textInput}
                         editable={false}
                         value={this.state.roundingMethod} />
-                </ModalSelector>
+        </ModalSelector>
       </View>
+    </View>
     );
   }
 }
 
 export default Limits;
+
+
+const styles= EStyleSheet.create({
+    container: {
+      flex: 1, //full screen
+      alignItems: 'center',
+      justifyContent:'space-between',
+      backgroundColor: '$blueBackground',
+      flexDirection:'column',
+      paddingVertical: 5,
+    },
+    itemContainer: {
+      width: '97.5%',
+      backgroundColor: '$white',
+      paddingVertical: 10,
+      borderRadius: 6,
+      paddingLeft: 5,
+      height: 117,
+      justifyContent:'space-around',
+    },
+    itemSubContainer: {
+      flexDirection:'row',
+      justifyContent:'space-around',
+      // width: '97.5%',
+      // backgroundColor: '$white',
+      // paddingVertical: 10,
+      // borderRadius: 6,
+      // paddingLeft: 5,
+      // height: 200,
+    },
+    questionStyle:{
+      fontFamily: "$textFont",
+      fontSize: 16,
+    },
+    valueStyle:{
+      fontFamily: "$textFont",
+      fontSize: 20,
+      paddingVertical: 6,
+    },
+    textInput:{
+      borderWidth:1,
+      borderColor:'#ccc',
+      padding:5,
+      height:30,
+      width:200,
+      alignSelf: 'center',
+    },
+    sliderContainer:{
+      width:250,
+    },
+});
