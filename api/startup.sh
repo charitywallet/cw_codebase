cbb9a5ff7e75bb19d3ccfd0ab1dc62499759f48c
9vinny <9vinny@gmail.com>
2019-05-10 18:39:56 -0700

    database moved to aws_rds

--- api/startup.sh
+++ api/startup.sh
@@ -1,19 +1,20 @@
-# startup.sh: script to create docker containers to run this async task queue
+# startup.sh: script to create docker containers to run this project
 
-# Create network
-docker network create lab8-network
+#clean up
+docker kill mysql-server cw-webserver
+docker rm mysql-server cw-webserver
 
-# Create container running redis
-docker run --network lab8-network --name lab8-redis -d redis redis-server --appendonly yes
+#create network
+docker network create cw-network
 
-# Build and create container running rq workers
-pushd workerserver
-docker build -t workerserver .
-docker run --network lab8-network --name lab8-workerserver -d workerserver
-popd
+#create my sql server container
+docker run -d --name cw-mysql-server --network cw-network -e MYSQL_ROOT_PASSWORD=capstone mysql --default-authentication-plugin=mysql_native_password
+docker exec -it cw-mysql-server bash
+# use command -  "mysql -h localhost -u root -p"  password - capstone to login to sql server
+# use sql in db_creation.sql to create db setup
 
 # Build and create container for our definition webserver
 pushd webserver
 docker build -t webserver .
-docker run --network lab8-network --name lab8-webserver -d -e FLASK_APP=webserver.py -p 5000:5000 webserver
+docker run --network cw-network --name cw-webserver -d -e FLASK_APP=webserver.py -p 5000:5000 webserver
 popd
